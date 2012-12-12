94fd6996f58047b685dc7ebf9e7c0632325ae79a
Wil Schultz <wschultz@bsdboy.com>
2012-12-11 16:39:41 -0800

    Added attributes, dher

---
+++ attributes/default.rb
@@ -0,0 +1,5 @@
+default[:gitolite][:admin_name]         = "git"
+default[:gitolite][:admin_home]         = "/var/lib/gitolite3"
+default[:gitolite][:repository_url]	= "git://github.com/sitaramc/gitolite.git"
+default[:gitolite][:install_dir]	= "/opt/gitolite"
+
