2e5033e242523d2bd31308a49b26c61dd7a72fe2
Wil Schultz <wschultz@bsdboy.com>
2012-12-11 16:39:22 -0800

    Added some further logic, seems to install now

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 maintainer       "Wil Schultz"
 maintainer_email "wschultz@bsdboy.com"
 license          "Apache 2.0"
-description      "Installs/Configures gitolite"
+description      "Installs/Configures gitolite3"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.0"
+version          "1.1.1"
 
 %w{ git }.each do |cb|
   depends cb
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,46 +7,68 @@
 
 include_recipe "git::default"
 
+githost = node['hostname']
+
 user "#{node[:gitolite][:admin_name]}" do
-  comment "Git User"
+  comment "Gitolite Admin User"
   shell "/bin/bash"
   home "#{node[:gitolite][:admin_home]}"
   supports :manage_home => true
 end
 
+directory "#{node[:gitolite][:install_dir]}" do
+  user "#{node[:gitolite][:admin_name]}"
+  group "#{node[:gitolite][:admin_name]}"
+  mode 00755
+  action :create
+  recursive true
+end
+
 git "#{node[:gitolite][:install_dir]}" do
   repository "#{node[:gitolite][:repository_url]}"
+  reference "master"
+  action :checkout
+  user "#{node[:gitolite][:admin_name]}"
+  group "#{node[:gitolite][:admin_name]}"
+end
+
+execute "ssh-keygen -q -f #{node[:gitolite][:admin_home]}/.ssh/id_rsa -N \"\"" do
+  user "#{node[:gitolite][:admin_name]}"
+  group "#{node[:gitolite][:admin_name]}"
+  action :run
+  creates "#{node[:gitolite][:admin_home]}/.ssh/id_rsa.pub"
+end
+
+directory "#{node[:gitolite][:install_dir]}/bin" do
+  user "#{node[:gitolite][:admin_name]}"
+  group "#{node[:gitolite][:admin_name]}"
+  mode 00755
+  action :create
+  recursive true
+end
+
+execute "./install -ln" do
+  user "#{node[:gitolite][:admin_name]}"
+  group "#{node[:gitolite][:admin_name]}"
+  environment ({"HOME" => "#{node[:gitolite][:install_dir]}"})
+  cwd "#{node[:gitolite][:install_dir]}"
+  #not_if {File.exists?("#{node[:gitolite][:install_dir]}/bin/gitolite") }
+  creates "#{node[:gitolite][:install_dir]}/bin/gitolite"
+end
+
+execute "bin/gitolite setup -pk #{node[:gitolite][:admin_home]}/.ssh/id_rsa.pub" do
+  user "#{node[:gitolite][:admin_name]}"
+  group "#{node[:gitolite][:admin_name]}"
+  environment ({"HOME" => "#{node[:gitolite][:admin_home]}", "USER" => "#{node[:gitolite][:admin_name]}"})
+  cwd "#{node[:gitolite][:install_dir]}"
+  not_if {File.exist? "#{node[:gitolite][:admin_home]}/.ssh/authorized_keys"}
+end
+
+git "#{node[:gitolite][:admin_home]}/gitolite-admin" do
+  repository "git@#{githost}:gitolite-admin"
   reference "master"
   action :sync
   user "#{node[:gitolite][:admin_name]}"
   group "#{node[:gitolite][:admin_name]}"
-end
-
-execute "ssh-keygen -q -f #{node[:gitolite][:admin_home]}/.ssh/id_rsa -N \"\" " do
-  user "#{node[:gitolite][:admin_name]}"
-  group "#{node[:gitolite][:admin_name]}"
-  action :run
-  not_if {File.exist? "#{node[:gitolite][:admin_home]}/.ssh/id_rsa"}
-end
-
-execute "cp #{node[:gitolite][:admin_home]}/.ssh/id_rsa.pub #{node[:gitolite][:admin_home]}/.ssh/authorized_keys" do
-  user "#{node[:gitolite][:admin_name]}"
-  group "#{node[:gitolite][:admin_name]}"
-  not_if {File.exist? "#{node[:gitolite][:admin_home]}/.ssh/authorized_keys"}
-end
-
-execute "gitolite/install -ln /usr/local/bin" do
-  user "root"
-  environment ({"HOME" => "#{node[:gitolite][:admin_home]}"})
-  cwd "#{node[:gitolite][:admin_home]}"
-  not_if {File.exists?("#{node[:gitolite][:admin_home]}/gitolite/src/VERSION") }
-end
-
-execute "gitolite setup -pk .ssh/id_rsa.pub" do
-  user "#{node[:gitolite][:admin_name]}"
-  group "#{node[:gitolite][:admin_name]}"
-  environment ({"HOME" => "#{node[:gitolite][:admin_home]}"})
-  cwd "#{node[:gitolite][:admin_home]}"
-  not_if {File.exist? "#{node[:gitolite][:admin_home]}/repositories"}
 end
 
