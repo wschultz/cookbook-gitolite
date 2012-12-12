fee6c8f11aaf4e7939f190bb42912249fed32108
Wil Schultz <wschultz@bsdboy.com>
2012-12-11 16:43:35 -0800

    attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,4 +2,3 @@ default[:gitolite][:admin_name]         = "git"
 default[:gitolite][:admin_home]         = "/var/lib/gitolite3"
 default[:gitolite][:repository_url]	= "git://github.com/sitaramc/gitolite.git"
 default[:gitolite][:install_dir]	= "/opt/gitolite"
-
